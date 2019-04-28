ca7d6e0cc511e0a8665e26dd2d6fe8c09a3801dc
zzsushiboyzz@gmail.com <zzsushiboyzz@gmail.com>
2019-04-28 09:53:48 +0100

    Implement Memento

--- src/designPattern/behavior/MementoPattern.java
+++ src/designPattern/behavior/MementoPattern.java
@@ -1,4 +1,37 @@
 package designPattern.behavior;
 
 public class MementoPattern {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Memento{
+
+}
+
+interface Originator{
+    public void restore(Memento memento);
+    public void createMemento();
+}
+
+class ConcreteOriginator implements Originator{
+
+    @Override
+    public void restore(Memento memento) {
+
+    }
+
+    @Override
+    public void createMemento() {
+
+    }
+
+    class ConcreteMemento implements Memento{
+
+    }
+}
+
+class CareTaker{
+
 }
